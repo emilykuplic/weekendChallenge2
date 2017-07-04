94e1e054e2404193d2b174d2523f70b40cd9106c
Emily Kuplic <emilykuplic@gmail.com>
2017-07-04 15:21:50 -0500

    half functional, not calculating result or sending back to client

---
+++ server/public/scripts/client.js
@@ -0,0 +1,47 @@
+var values = [];
+
+$(document).ready(function(){
+  console.log('client.js sourced');
+
+  $('#calculatorResults').hide();
+
+  // take user radio selection with click listener and pass to server
+  $('#mathResults').on('click', function(){
+
+
+    // selecting math type from setup and setting as var
+    mathType = $('input:radio[name=math]:checked').val();
+    var selection = $('radio').val();
+    console.log('submit button clicked');
+    values = [];
+    var mathX = $('#x').val();
+    var mathY = $('#y').val();
+    values.push(mathX, mathY);
+    console.log(values);
+
+
+    $.ajax({
+      type: 'POST',
+      url: '/math',   // address route for getting/putting data from server
+      data: {mathType: mathType},
+
+      success: function(response) {
+        console.log("request received");
+        console.log(response);
+        // connect up button
+      }
+    });
+
+    $.ajax({
+      //Sending x & y values to the server.
+      type: 'POST',
+      url: '/numbers',
+      data: {values: values},
+      success: function(response) {
+        console.log("x & y sent");
+        console.log(response.resultsArray);
+      }
+    });
+
+  });
+});
---
+++ server/server.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var port = 7000;
+var bodyParser = require('body-parser');
+
+var valuesArray = [];
+var math = '';
+var result = '';
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post("/math", function (req, res){
+  var math = req.body;
+  console.log('received math type', math.mathType);
+  // Checking to see what our max number is ... Its a string... Which doesnt work in our function
+  console.log(typeof math.mathType);
+  // since we have to respond this is our response message
+  res.send({message: 'Math type now stored on server'});
+ console.log(math);
+});
+
+app.post("/numbers", function (req, res) {
+  //Here we are receiving our guessesArray from the client.js
+  valuesArray = req.body;
+  console.log('received valuesArray', valuesArray.values);
+  // // run checkGuesses to see if they're right/low/high
+  mathResults();
+  console.log(result);
+  res.send({result});
+});
+
+
+// Catch all
+app.get('/*', function(req, res){
+  // http://localhost:5002/scripts/client.js
+  // scripts/client.js === req.params[0];
+  var file = req.params[0] || '/views/index.html';
+  res.sendFile(path.join(__dirname, '/public/', file));
+});
+
+app.listen(port, function(){
+  console.log('Server running on port', port);
+});
+
+// functions
+// a function to check do math based on valuesArray & mathType variable
+function mathResults() {
+  // result = 0;
+    if (math.mathType == "add")
+    { result = (Number(valuesArray.values[0])) + (Number(valuesArray.values[1]));
+    }
+    else if (math.mathType == "subtract")
+    {result = (Number(valuesArray.values[0])) - (Number(valuesArray.values[1]));
+    }
+    else if (math.mathType == "multiply")
+    {result = (Number(valuesArray.values[0])) * (Number(valuesArray.values[1]));
+    }
+    else if (math.mathType == "divide")
+    {result = (Number(valuesArray.values[0])) / (Number(valuesArray.values[1]));
+    }
+  }
